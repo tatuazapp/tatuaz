fc517966bc89d2552758384c81d26a89655480ff
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2022-09-11 00:29:30 +0200

    fix: file linting

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -22,7 +22,7 @@ export function Index() {
             </h1>
           </div>
 
-          <div id="hero" className="rounded">
+          <div className="rounded" id="hero">
             <div className="text-container">
               <h2>
                 <svg
@@ -32,10 +32,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
                   />
                 </svg>
                 <span>You&apos;re up and running</span>
@@ -55,13 +55,13 @@ export function Index() {
           </div>
 
           <div id="middle-content">
-            <div id="learning-materials" className="rounded shadow">
+            <div className="rounded shadow" id="learning-materials">
               <h2>Learning materials</h2>
               <a
-                href="https://nx.dev/getting-started/intro?utm_source=nx-project"
-                target="_blank"
-                rel="noreferrer"
                 className="list-item-link"
+                href="https://nx.dev/getting-started/intro?utm_source=nx-project"
+                rel="noreferrer"
+                target="_blank"
               >
                 <svg
                   fill="none"
@@ -70,10 +70,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                   />
                 </svg>
                 <span>
@@ -87,18 +87,18 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M9 5l7 7-7 7"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M9 5l7 7-7 7"
                   />
                 </svg>
               </a>
               <a
-                href="https://blog.nrwl.io/?utm_source=nx-project"
-                target="_blank"
-                rel="noreferrer"
                 className="list-item-link"
+                href="https://blog.nrwl.io/?utm_source=nx-project"
+                rel="noreferrer"
+                target="_blank"
               >
                 <svg
                   fill="none"
@@ -107,10 +107,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
                   />
                 </svg>
                 <span>
@@ -124,23 +124,23 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M9 5l7 7-7 7"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M9 5l7 7-7 7"
                   />
                 </svg>
               </a>
               <a
-                href="https://www.youtube.com/c/Nrwl_io/videos?utm_source=nx-project&sub_confirmation=1"
-                target="_blank"
-                rel="noreferrer"
                 className="list-item-link"
+                href="https://www.youtube.com/c/Nrwl_io/videos?utm_source=nx-project&sub_confirmation=1"
+                rel="noreferrer"
+                target="_blank"
               >
                 <svg
+                  fill="currentColor"
                   role="img"
                   viewBox="0 0 24 24"
-                  fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <title>YouTube</title>
@@ -157,18 +157,18 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M9 5l7 7-7 7"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M9 5l7 7-7 7"
                   />
                 </svg>
               </a>
               <a
-                href="https://nx.dev/react-tutorial/01-create-application?utm_source=nx-project"
-                target="_blank"
-                rel="noreferrer"
                 className="list-item-link"
+                href="https://nx.dev/react-tutorial/01-create-application?utm_source=nx-project"
+                rel="noreferrer"
+                target="_blank"
               >
                 <svg
                   fill="none"
@@ -177,10 +177,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
                   />
                 </svg>
                 <span>
@@ -194,18 +194,18 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M9 5l7 7-7 7"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M9 5l7 7-7 7"
                   />
                 </svg>
               </a>
               <a
-                href="https://nxplaybook.com/?utm_source=nx-project"
-                target="_blank"
-                rel="noreferrer"
                 className="list-item-link"
+                href="https://nxplaybook.com/?utm_source=nx-project"
+                rel="noreferrer"
+                target="_blank"
               >
                 <svg
                   fill="none"
@@ -216,10 +216,10 @@ export function Index() {
                   <path d="M12 14l9-5-9-5-9 5 9 5z" />
                   <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
                   <path
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                   />
                 </svg>
                 <span>
@@ -233,21 +233,21 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M9 5l7 7-7 7"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M9 5l7 7-7 7"
                   />
                 </svg>
               </a>
             </div>
             <div id="other-links">
               <a
-                id="nx-console"
                 className="button-pill rounded shadow"
                 href="https://marketplace.visualstudio.com/items?itemName=nrwl.angular-console&utm_source=nx-project"
-                target="_blank"
+                id="nx-console"
                 rel="noreferrer"
+                target="_blank"
               >
                 <svg
                   fill="currentColor"
@@ -263,11 +263,11 @@ export function Index() {
                   <span>Plugin for VSCode</span>
                 </span>
               </a>
-              <div id="nx-cloud" className="rounded shadow">
+              <div className="rounded shadow" id="nx-cloud">
                 <div>
                   <svg
-                    viewBox="0 0 120 120"
                     fill="none"
+                    viewBox="0 0 120 120"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
@@ -291,19 +291,19 @@ export function Index() {
                 <pre>nx connect-to-nx-cloud</pre>
                 <a
                   href="https://nx.app/?utm_source=nx-project"
-                  target="_blank"
                   rel="noreferrer"
+                  target="_blank"
                 >
                   {' '}
                   What is Nx Cloud?{' '}
                 </a>
               </div>
               <a
-                id="nx-repo"
                 className="button-pill rounded shadow"
                 href="https://github.com/nrwl/nx?utm_source=nx-project"
-                target="_blank"
+                id="nx-repo"
                 rel="noreferrer"
+                target="_blank"
               >
                 <svg
                   fill="currentColor"
@@ -321,7 +321,7 @@ export function Index() {
             </div>
           </div>
 
-          <div id="commands" className="rounded shadow">
+          <div className="rounded shadow" id="commands">
             <h2>Next steps</h2>
             <p>Here are some things you can do with Nx:</p>
             <details>
@@ -333,10 +333,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
                 Add UI library
@@ -357,10 +357,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
                 View interactive project graph
@@ -376,10 +376,10 @@ export function Index() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+                    d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
                 Run affected commands
@@ -404,10 +404,10 @@ export function Index() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               />
             </svg>
           </p>
