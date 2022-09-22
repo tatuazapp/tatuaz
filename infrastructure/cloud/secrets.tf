1d0d1fa0bf621e10da7f8351b051e818af22e5fb
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2022-09-22 23:11:23 +0200

    fix: format

--- infrastructure/cloud/secrets.tf
+++ infrastructure/cloud/secrets.tf
@@ -5,8 +5,8 @@ resource "kubernetes_secret" "main" {
   }
 
   data = {
-    db_username = azurerm_postgresql_server.main.administrator_login
-    db_password = azurerm_postgresql_server.main.administrator_login_password
+    db_username          = azurerm_postgresql_server.main.administrator_login
+    db_password          = azurerm_postgresql_server.main.administrator_login_password
     sb_connection_string = azurerm_servicebus_namespace.main.default_primary_connection_string
   }
 }
