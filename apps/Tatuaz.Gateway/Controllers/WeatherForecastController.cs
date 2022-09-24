04bb2815807466ba4693e6fce69ad790f3fdb520
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2022-09-24 09:56:50 +0200

    task: Enable swagger in production and add configuration options

--- apps/Tatuaz.Gateway/Controllers/WeatherForecastController.cs
+++ apps/Tatuaz.Gateway/Controllers/WeatherForecastController.cs
@@ -18,11 +18,14 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
+    /// <summary>
+    ///     Test xml comment
+    /// </summary>
+    /// <returns>test</returns>
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
--- apps/Tatuaz.Gateway/Program.cs
+++ apps/Tatuaz.Gateway/Program.cs
@@ -1,25 +1,11 @@
+using Tatuaz.Gateway.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddConfiguration(builder.Configuration);
+builder.Services.AddGatewayServices(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.ConfigurePipeline();
 app.Run();
