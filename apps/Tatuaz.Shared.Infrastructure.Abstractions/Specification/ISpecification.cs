091a6561c8158c78fdb9ef5f120006284751c539
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2022-09-24 10:39:12 +0200

    task: add ISpecification

---
+++ apps/Tatuaz.Shared.Infrastructure.Abstractions/Specification/ISpecification.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Tatuaz.Shared.Infrastructure.Abstractions;
+
+public interface ISpecification<T>
+{
+    public Func<IQueryable<T>, IQueryable<T>>? ApplyTracking();
+    public Func<IQueryable<T>, IQueryable<T>>? ApplyFiltering();
+    public Func<IQueryable<T>, IQueryable<T>>? ApplyOrdering();
+    public Func<IQueryable<T>, IQueryable<T>>? ApplyIncludes();
+    public Func<IQueryable<T>, IQueryable<T>>? ApplyPaging();
+}
