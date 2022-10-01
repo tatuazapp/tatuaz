e3937b636d2c186b9483ecae2bb0b3582ba93dad
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2022-10-01 22:39:46 +0200

    fix: codeowners + typo fix

--- libs/Tatuaz.Testing.Fakes/Common/PrimitiveValuesGenerator.cs
+++ libs/Tatuaz.Testing.Fakes/Common/PrimitiveValuesGenerator.cs
@@ -4,7 +4,7 @@ namespace Tatuaz.Testing.Fakes.Common;
 
 public class PrimitiveValuesGenerator : IPrimitiveValuesGenerator
 {
-    private readonly Instant[] _dateTimes;
+    private readonly Instant[] _instants;
     private readonly decimal[] _decimals;
     private readonly double[] _doubles;
     private readonly float[] _floats;
@@ -15,7 +15,7 @@ public class PrimitiveValuesGenerator : IPrimitiveValuesGenerator
     public PrimitiveValuesGenerator()
     {
         _guids = new Guid[10];
-        _dateTimes = new Instant[10];
+        _instants = new Instant[10];
         _strings = new string[10];
         _ints = new int[10];
         _floats = new float[10];
@@ -25,7 +25,7 @@ public class PrimitiveValuesGenerator : IPrimitiveValuesGenerator
         for (var i = 0; i < 10; i++)
         {
             _guids[i] = Guid.NewGuid();
-            _dateTimes[i] = Instant.FromUtc(2020, 1, 1, 0, 0).Plus(Duration.FromMilliseconds(Random.Shared.Next()));
+            _instants[i] = Instant.FromUtc(2020, 1, 1, 0, 0).Plus(Duration.FromMilliseconds(Random.Shared.Next()));
             _strings[i] = Guid.NewGuid().ToString();
             _ints[i] = Random.Shared.Next();
             _floats[i] = Random.Shared.NextSingle() * 2048f - 1024f;
@@ -43,7 +43,7 @@ public class PrimitiveValuesGenerator : IPrimitiveValuesGenerator
     public Instant Instants(int index)
     {
         if (index < 0 || index >= 10) throw new ArgumentException(nameof(index));
-        return _dateTimes[index];
+        return _instants[index];
     }
 
     public string Strings(int index)
