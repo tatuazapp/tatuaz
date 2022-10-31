06c6a794511f5b11ca96e9cd99df6856943b0c98
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2022-10-31 11:49:33 +0100

    task: add authorization header to swagger

--- apps/Tatuaz.Gateway/Configuration/ServiceExtensions.cs
+++ apps/Tatuaz.Gateway/Configuration/ServiceExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Security.Claims;
@@ -101,6 +102,35 @@ public static class ServiceExtensions
     {
         services.AddSwaggerGen(opt =>
         {
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+
+                    },
+                    new List<string>()
+                }
+            });
             opt.SchemaFilter<FluentValidationSchemaFilter>();
             opt.SwaggerDoc(
                 "v1",
