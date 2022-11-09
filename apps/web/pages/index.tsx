84be93e8c3b8a2beb44f5a0c1a94d66f8f9d8934
Nikodem Wojcik <01nikodemw@gmail.com>
2022-11-09 20:42:16 +0100

    fix: styling

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,7 @@
 import AppLayout from "../components/auth/AppLayout"
+import { PageContentWrapper } from "../components/common/PageContentWrapper/styles"
 import PhotoCardSection from "../components/home/PhotoCardSection"
 import StatsSection from "../components/home/StatsSection"
-import { PageContentWrapper } from "./styles"
 
 const Index = () => (
   <AppLayout>
--- apps/web/pages/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components"
-
-export const PageContentWrapper = styled.div`
-  margin-left: ${({ theme }) => theme.space.xlarge};
-  margin-right: ${({ theme }) => theme.space.xlarge}; ;
-`
