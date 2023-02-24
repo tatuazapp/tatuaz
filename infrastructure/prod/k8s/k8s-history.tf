23cdefb3b41e4a3125310f93b719587936ffa09a
Łukasz Sobczak <lukasz.k.sobczak@gmail.com>
2023-02-24 12:44:15 +0100

    Fix pooling

--- infrastructure/prod/k8s/k8s-history.tf
+++ infrastructure/prod/k8s/k8s-history.tf
@@ -54,6 +54,6 @@ resource "kubernetes_secret" "k8s_history" {
     RabbitMq__VirtualHost           = "/"
     RabbitMq__Username              = var.k8s_queue.default_user
     RabbitMq__Password              = var.k8s_queue.default_password
-    ConnectionStrings__TatuazHistDb = "Server=postgres-service-lb.tatuaz.svc.cluster.local;Port=5432;Database=TatuazHistDb;User Id=${var.k8s_postgres.admin_login};Password=${var.k8s_postgres.admin_password};"
+    ConnectionStrings__TatuazHistDb = "Server=postgres-service-lb.tatuaz.svc.cluster.local;Port=5432;Database=TatuazHistDb;User Id=${var.k8s_postgres.admin_login};Password=${var.k8s_postgres.admin_password};Pooling=true;MinPoolSize=0;MaxPoolSize=10;"
   }
 }
