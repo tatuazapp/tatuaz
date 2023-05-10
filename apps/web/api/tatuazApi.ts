bd728b992acca480f9bf8ca474279c91807124b6
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-05-10 21:05:36 +0200

    Lint

--- apps/web/api/tatuazApi.ts
+++ apps/web/api/tatuazApi.ts
@@ -260,16 +260,22 @@ export interface FullRequestParams extends Omit<RequestInit, "body"> {
   cancelToken?: CancelToken
 }
 
-export type RequestParams = Omit<FullRequestParams, "body" | "method" | "query" | "path">
+export type RequestParams = Omit<
+  FullRequestParams,
+  "body" | "method" | "query" | "path"
+>
 
 export interface ApiConfig<SecurityDataType = unknown> {
   baseUrl?: string
   baseApiParams?: Omit<RequestParams, "baseUrl" | "cancelToken" | "signal">
-  securityWorker?: (securityData: SecurityDataType | null) => Promise<RequestParams | void> | RequestParams | void
+  securityWorker?: (
+    securityData: SecurityDataType | null
+  ) => Promise<RequestParams | void> | RequestParams | void
   customFetch?: typeof fetch
 }
 
-export interface HttpResponse<D extends unknown, E extends unknown = unknown> extends Response {
+export interface HttpResponse<D extends unknown, E extends unknown = unknown>
+  extends Response {
   data: D
   error: E
 }
@@ -287,7 +293,8 @@ export class HttpClient<SecurityDataType = unknown> {
   private securityData: SecurityDataType | null = null
   private securityWorker?: ApiConfig<SecurityDataType>["securityWorker"]
   private abortControllers = new Map<CancelToken, AbortController>()
-  private customFetch = (...fetchParams: Parameters<typeof fetch>) => fetch(...fetchParams)
+  private customFetch = (...fetchParams: Parameters<typeof fetch>) =>
+    fetch(...fetchParams)
 
   private baseApiParams: RequestParams = {
     credentials: "same-origin",
@@ -306,7 +313,9 @@ export class HttpClient<SecurityDataType = unknown> {
 
   protected encodeQueryParam(key: string, value: any) {
     const encodedKey = encodeURIComponent(key)
-    return `${encodedKey}=${encodeURIComponent(typeof value === "number" ? value : `${value}`)}`
+    return `${encodedKey}=${encodeURIComponent(
+      typeof value === "number" ? value : `${value}`
+    )}`
   }
 
   protected addQueryParam(query: QueryParamsType, key: string) {
@@ -320,9 +329,15 @@ export class HttpClient<SecurityDataType = unknown> {
 
   protected toQueryString(rawQuery?: QueryParamsType): string {
     const query = rawQuery || {}
-    const keys = Object.keys(query).filter((key) => "undefined" !== typeof query[key])
+    const keys = Object.keys(query).filter(
+      (key) => "undefined" !== typeof query[key]
+    )
     return keys
-      .map((key) => (Array.isArray(query[key]) ? this.addArrayQueryParam(query, key) : this.addQueryParam(query, key)))
+      .map((key) =>
+        Array.isArray(query[key])
+          ? this.addArrayQueryParam(query, key)
+          : this.addQueryParam(query, key)
+      )
       .join("&")
   }
 
@@ -333,7 +348,9 @@ export class HttpClient<SecurityDataType = unknown> {
 
   private contentFormatters: Record<ContentType, (input: any) => any> = {
     [ContentType.Json]: (input: any) =>
-      input !== null && (typeof input === "object" || typeof input === "string") ? JSON.stringify(input) : input,
+      input !== null && (typeof input === "object" || typeof input === "string")
+        ? JSON.stringify(input)
+        : input,
     [ContentType.FormData]: (input: any) =>
       Object.keys(input || {}).reduce((formData, key) => {
         const property = input[key]
@@ -343,14 +360,17 @@ export class HttpClient<SecurityDataType = unknown> {
             ? property
             : typeof property === "object" && property !== null
             ? JSON.stringify(property)
-            : `${property}`,
+            : `${property}`
         )
         return formData
       }, new FormData()),
     [ContentType.UrlEncoded]: (input: any) => this.toQueryString(input),
   }
 
-  protected mergeRequestParams(params1: RequestParams, params2?: RequestParams): RequestParams {
+  protected mergeRequestParams(
+    params1: RequestParams,
+    params2?: RequestParams
+  ): RequestParams {
     return {
       ...this.baseApiParams,
       ...params1,
@@ -363,7 +383,9 @@ export class HttpClient<SecurityDataType = unknown> {
     }
   }
 
-  protected createAbortSignal = (cancelToken: CancelToken): AbortSignal | undefined => {
+  protected createAbortSignal = (
+    cancelToken: CancelToken
+  ): AbortSignal | undefined => {
     if (this.abortControllers.has(cancelToken)) {
       const abortController = this.abortControllers.get(cancelToken)
       if (abortController) {
@@ -407,15 +429,27 @@ export class HttpClient<SecurityDataType = unknown> {
     const payloadFormatter = this.contentFormatters[type || ContentType.Json]
     const responseFormat = format || requestParams.format
 
-    return this.customFetch(`${baseUrl || this.baseUrl || ""}${path}${queryString ? `?${queryString}` : ""}`, {
-      ...requestParams,
-      headers: {
-        ...(requestParams.headers || {}),
-        ...(type && type !== ContentType.FormData ? { "Content-Type": type } : {}),
-      },
-      signal: cancelToken ? this.createAbortSignal(cancelToken) : requestParams.signal,
-      body: typeof body === "undefined" || body === null ? null : payloadFormatter(body),
-    }).then(async (response) => {
+    return this.customFetch(
+      `${baseUrl || this.baseUrl || ""}${path}${
+        queryString ? `?${queryString}` : ""
+      }`,
+      {
+        ...requestParams,
+        headers: {
+          ...(requestParams.headers || {}),
+          ...(type && type !== ContentType.FormData
+            ? { "Content-Type": type }
+            : {}),
+        },
+        signal: cancelToken
+          ? this.createAbortSignal(cancelToken)
+          : requestParams.signal,
+        body:
+          typeof body === "undefined" || body === null
+            ? null
+            : payloadFormatter(body),
+      }
+    ).then(async (response) => {
       const r = response as HttpResponse<T, E>
       r.data = null as unknown as T
       r.error = null as unknown as E
@@ -452,7 +486,9 @@ export class HttpClient<SecurityDataType = unknown> {
  *
  * API for tatuaz.app
  */
-export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
+export class Api<
+  SecurityDataType extends unknown
+> extends HttpClient<SecurityDataType> {
   identity = {
     /**
      * No description
@@ -520,7 +556,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
         /** @format binary */
         photo?: File
       },
-      params: RequestParams = {},
+      params: RequestParams = {}
     ) =>
       this.request<EmptyResponse, ErrorResponse | EmptyResponse>({
         path: `/Identity/SetForegroundPhoto`,
@@ -551,7 +587,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
         /** @format binary */
         photo?: File
       },
-      params: RequestParams = {},
+      params: RequestParams = {}
     ) =>
       this.request<EmptyResponse, ErrorResponse | EmptyResponse>({
         path: `/Identity/SetBackgroundPhoto`,
@@ -577,7 +613,10 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
      * @response `403` `EmptyResponse` Forbidden
      * @response `500` `ErrorResponse` Server Error
      */
-    deleteForegroundPhoto: (data: DeleteForegroundPhotoDto, params: RequestParams = {}) =>
+    deleteForegroundPhoto: (
+      data: DeleteForegroundPhotoDto,
+      params: RequestParams = {}
+    ) =>
       this.request<EmptyResponse, ErrorResponse | EmptyResponse>({
         path: `/Identity/DeleteForegroundPhoto`,
         method: "POST",
@@ -602,7 +641,10 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
      * @response `403` `EmptyResponse` Forbidden
      * @response `500` `ErrorResponse` Server Error
      */
-    deleteBackgroundPhoto: (data: DeleteBackgroundPhotoDto, params: RequestParams = {}) =>
+    deleteBackgroundPhoto: (
+      data: DeleteBackgroundPhotoDto,
+      params: RequestParams = {}
+    ) =>
       this.request<EmptyResponse, ErrorResponse | EmptyResponse>({
         path: `/Identity/DeleteBackgroundPhoto`,
         method: "POST",
@@ -703,7 +745,10 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
      * @response `500` `ErrorResponse` Server Error
      */
     getTopArtists: (data: GetTopArtistsDto, params: RequestParams = {}) =>
-      this.request<OkResponsePagedDataBriefArtistDto, ErrorResponse | EmptyResponse>({
+      this.request<
+        OkResponsePagedDataBriefArtistDto,
+        ErrorResponse | EmptyResponse
+      >({
         path: `/Identity/GetTopArtists`,
         method: "POST",
         body: data,
@@ -728,7 +773,10 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
      * @response `500` `ErrorResponse` Server Error
      */
     listCategories: (data: ListCategoriesDto, params: RequestParams = {}) =>
-      this.request<OkResponsePagedDataCategoryDto, ErrorResponse | EmptyResponse>({
+      this.request<
+        OkResponsePagedDataCategoryDto,
+        ErrorResponse | EmptyResponse
+      >({
         path: `/Photo/ListCategories`,
         method: "POST",
         body: data,
@@ -757,7 +805,7 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
       data: {
         Photos?: File[]
       },
-      params: RequestParams = {},
+      params: RequestParams = {}
     ) =>
       this.request<OkResponseUploadedPhotosDto, ErrorResponse | EmptyResponse>({
         path: `/Post/UploadPostPhotos`,
