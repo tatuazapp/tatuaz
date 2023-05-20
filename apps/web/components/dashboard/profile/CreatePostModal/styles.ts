367767a0711881e4105cb524e9f5afb2d24e3f02
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-05-20 17:57:43 +0200

    fix: improve photo upload UX

--- apps/web/components/dashboard/profile/CreatePostModal/styles.ts
+++ apps/web/components/dashboard/profile/CreatePostModal/styles.ts
@@ -3,7 +3,11 @@ import styled from "styled-components"
 
 export const UploadedPhotosWrapper = styled.div`
   overflow: auto;
-  max-height: 160px;
+  display: grid;
+  grid-gap: ${({ theme }) => theme.space.small};
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+
+  max-height: 460px;
   ${down("lg")} {
     padding-right: ${({ theme }) => theme.space.small};
     padding-left: ${({ theme }) => theme.space.small};
